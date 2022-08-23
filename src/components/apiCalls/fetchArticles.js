f143844b1d477a097545f2a51750102a2bd31e6f
Skahrr <1dgonascaaron@gmail.com>
2022-08-23 14:45:57 +0100

    fetching using axios params

--- src/components/apiCalls/fetchArticles.js
+++ src/components/apiCalls/fetchArticles.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
 export const fetchArticles = (endpoint) => {
-    let query = ''
-    if(endpoint){
-        query = `?topic=${endpoint}`
-    }
+   
   return axios
-    .get(`https://be-nc-news-aaron.herokuapp.com/api/articles${query}`)
+    .get(`https://be-nc-news-aaron.herokuapp.com/api/articles`, {params: {topic: endpoint}})
     .then(({ data }) => {
       return data.articles;
     });
