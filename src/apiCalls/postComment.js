4bfe14cd32b04e3c939d96f3f3018d73ed256063
Skahrr <1dgonascaaron@gmail.com>
2022-08-25 18:09:31 +0200

    posting comments correctly

---
+++ src/apiCalls/postComment.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const request = axios.create({
+    baseURL: 'https://be-nc-news-aaron.herokuapp.com/api/'
+})
+
+export const postComment = (article_id, commentBody)=>{
+    console.log(article_id,);
+    return request.post(`articles/${article_id}/comments`, {username: 'jessjelly', body: commentBody}).then(({data})=>{
+        return data.comment
+    })
+}
