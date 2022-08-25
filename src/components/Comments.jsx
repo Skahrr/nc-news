0b9485b38b9b0fa08c98ed4846d5571d7f43cd5a
Skahrr <1dgonascaaron@gmail.com>
2022-08-25 12:30:06 +0200

    fetch comments

---
+++ src/components/Comments.jsx
@@ -0,0 +1,26 @@
+import { fetchComments } from "./apiCalls/fetchComments";
+import { useState, useEffect } from "react";
+
+export const Comments = ({ article_id }) => {
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    fetchComments(article_id).then((comments) => {
+      setComments(comments);
+    });
+  });
+
+  return (
+    <div className="article-comments">
+      <h3>All comments</h3>
+      {comments.map((comment) => {
+        return (
+          <div className="single-comment">
+            <h4>{comment.author}:</h4>
+            <span>Posted at: {comment.created_at}</span>
+            <p>{comment.body}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
