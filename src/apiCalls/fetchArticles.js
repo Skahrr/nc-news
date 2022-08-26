a1f4abef22ebd16f0d1083b5474f3b46e2bb62ec
Skahrr <1dgonascaaron@gmail.com>
2022-08-26 11:34:54 +0100

    console logs

--- src/apiCalls/fetchArticles.js
+++ src/apiCalls/fetchArticles.js
@@ -5,7 +5,6 @@ export const fetchArticles = (topic, sort_by, order) => {
   return axios
     .get(`https://be-nc-news-aaron.herokuapp.com/api/articles`, {params: {topic, sort_by, order}})
     .then(({ data }) => {
-      console.log(data.articles);
       return data.articles;
     });
 };
