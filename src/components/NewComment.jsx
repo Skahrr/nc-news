0b77593501eb3622e982fbaaba6bbc4b4f86408a
Skahrr <1dgonascaaron@gmail.com>
2022-08-26 15:51:26 +0100

    delete and posting not being optimistic anymore

--- src/components/NewComment.jsx
+++ src/components/NewComment.jsx
@@ -1,25 +1,17 @@
 import { useState } from "react";
 import { postComment } from "../apiCalls/postComment";
-export const NewComment = ({ article_id, comments, setComments }) => {
+export const NewComment = ({ article_id, comments, setComments, setIsPosted }) => {
   const [commentBody, setCommentBody] = useState("");
-  const [optimisticCommentID, setOptimisticID] = useState(382)
   const handleBody = (e) => {
     setCommentBody(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const optimisticComment = {
-      comment_id: optimisticCommentID,
-      body: commentBody,
-      author: 'jessjelly',
-      votes: 0
-    }
-    setComments([optimisticComment, ...comments])
-    setOptimisticID((currID)=>{
-      return currID + 1
-    })
-    postComment(article_id, commentBody);
+    setIsPosted(true)
+    postComment(article_id, commentBody).then(()=>{
+      setIsPosted(false)
+    });
   };
   return (
     <form onSubmit={handleSubmit}>
