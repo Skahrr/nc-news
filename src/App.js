c59cc2adaa5959540ea58e888408532c3dfde3ec
Skahrr <1dgonascaaron@gmail.com>
2022-08-26 12:27:59 +0100

    deleting comments properly

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import React from "react";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
+import { useState } from "react";
 import { Routes, BrowserRouter, Route, Navigate } from "react-router-dom";
 import { SingleArticle } from "./components/SingleArticle";
-
+import { UserContext } from "./contexts/UserContext";
 
 function App() {
-  
+  const [user, setUser] = useState("jessjelly");
   return (
-    
+    <UserContext.Provider value={{ user, setUser }}>
       <BrowserRouter>
         <div className="App">
           <Header />
@@ -18,11 +19,15 @@ function App() {
             <Route path="/" element={<Navigate to="/articles" />} />
             <Route path="/articles/:topic" element={<Main />} />
             <Route path="/articles" element={<Main />} />
-            <Route path="/articles/:topic/:article_id" element={<SingleArticle/>}/>
+            <Route
+              path="/articles/:topic/:article_id"
+              element={<SingleArticle />}
+            />
           </Routes>
           <Footer />
         </div>
       </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,9 +1,18 @@
 import { fetchComments } from "../apiCalls/fetchComments";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { NewComment } from "./NewComment";
+import { UserContext } from "../contexts/UserContext";
+import { deleteComment } from "../apiCalls/deleteComment";
 
 export const Comments = ({ article_id }) => {
   const [comments, setComments] = useState([]);
+  const {user} = useContext(UserContext)
+
+
+  const handleClick = (comment_id)=>{
+    deleteComment(comment_id);
+    
+  }
   useEffect(() => {
     fetchComments(article_id).then((comments) => {
       comments.sort((a,b)=>{
@@ -21,12 +30,16 @@ export const Comments = ({ article_id }) => {
     <div className="article-comments">
       <h3>Comments</h3>
     <NewComment article_id={article_id}/>
+    
       {comments.map((comment) => {
+        
         return (
           <div className="single-comment" key={comment.comment_id}>
             <h4>{comment.author}:</h4>
             <span>Posted at: {comment.created_at}</span>
             <p>{comment.body}</p>
+            
+            {comment.author===user && <button onClick={()=>{handleClick(comment.comment_id)}}>Delete</button>}
           </div>
         );
       })}
