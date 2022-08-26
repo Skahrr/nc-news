a8a489ff310d1ee2c2a622364e72eb660369f13f
Skahrr <1dgonascaaron@gmail.com>
2022-08-26 11:16:43 +0100

    fixed select

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -11,14 +11,14 @@ export const Filter = ({setSortBy, setOrder}) => {
   return (
     <div>
       <h4>Filter:</h4>
-      <select name="" id="sort_by" onChange={handleSortedChange}>
+      <select name="sort_by" id="sort_by" onChange={handleSortedChange}>
         <option value="created_at">Latest posts</option>
         <option value="title">Title</option>
         <option value="author">Author</option>
         <option value="votes">Votes</option>
         <option value="topic">Topic</option>
       </select>
-      <select name="" id="order" onChange={handleOrderChange}>
+      <select name="order" id="order" onChange={handleOrderChange}>
         <option value="DESC">Descending</option>
         <option value="ASC">Ascending</option>
       </select>
