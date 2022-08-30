984de7f942ac1df21e5f315165d4ae1bda553983
Skahrr <1dgonascaaron@gmail.com>
2022-08-30 08:12:16 +0100

    minimum style to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Menu } from "./Menu";
 export const Header = () => {
   return (
     <header>
-      <div id="title">NC TEST</div>
+      <div id="title">NC NEWS</div>
       <Menu />
     </header>
   );
