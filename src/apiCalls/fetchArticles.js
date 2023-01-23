ce94796587f8d638282cb40e599232b6b6804a00
Skahrr <1dgonascaaron@gmail.com>
2023-01-23 01:29:36 +0000

    test

--- src/apiCalls/fetchArticles.js
+++ src/apiCalls/fetchArticles.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const fetchArticles = (topic, sort_by, order) => {
    
   return axios
-    .get(`https://be-nc-news-aaron.herokuapp.com/api/articles`, {params: {topic, sort_by, order}})
+    .get(`https://be-nc-news-x3yy.onrender.com/api/articles`, {params: {topic, sort_by, order}})
     .then(({ data }) => {
       return data.articles;
     });
